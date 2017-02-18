d804b5a0a2d528c81a38b4dd7e82f8c4e6e599c6
ses10 <sesma@live.com>
2017-02-18 15:29:41 -0500

    output doc

--- week1/driver.py
+++ week1/driver.py
@@ -21,4 +21,20 @@ Example Script Execution
 -----------------
 python driver.py bfs 0,8,7,6,5,4,3,2,1
 
+Output
+------
+When executed, the program will write to a file called output.txt, containing the following statistics:
+
+path_to_goal: the sequence of moves taken to reach the goal
+cost_of_path: the number of moves taken to reach the goal
+nodes_expanded: the number of nodes that have been expanded
+fringe_size: the size of the frontier set when the goal node is found
+max_fringe_size: the maximum size of the frontier set in the lifetime of the algorithm
+search_depth: the depth within the search tree when the goal node is found
+max_search_depth:  the maximum depth of the search tree in the lifetime of the algorithm
+running_time: the total running time of the search instance, reported in seconds
+max_ram_usage: the maximum RAM usage in the lifetime of the process reported in megabytes
+
 """
+
+
