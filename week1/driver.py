ab544e1735abcd3c92a08fa09d0f15e2dc736298
ses10 <sesma@live.com>
2017-02-18 15:26:58 -0500

    inital doc

---
+++ week1/driver.py
@@ -0,0 +1,24 @@
+"""
+AI agent that solves the n-sliding puzzle game through various search algorithms
+
+Starting the script
+-------------------
+python driver.py <method> <board>
+
+Parameters
+----------
+The method argument will be one of the following.
+bfs (Breadth-First Search)
+dfs (Depth-First Search)
+ast (A-Star Search)
+ida (IDA-Star Search)
+
+The board argument will be a comma-separated list of integers containing no spaces that represents
+that initial state of the puzzle board
+
+
+Example Script Execution
+-----------------
+python driver.py bfs 0,8,7,6,5,4,3,2,1
+
+"""
